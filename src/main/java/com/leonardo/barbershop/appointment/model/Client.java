18c8c8ba569a41072f5abc981004fb6514a10a5f
Leonardo <leobarros_@outlook.com>
2026-05-15 00:01:18 -0300

    feat: add Client entity

---
+++ src/main/java/com/leonardo/barbershop/appointment/model/Client.java
@@ -0,0 +1,44 @@
+package com.leonardo.barbershop.appointment.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Table(name = "clients")
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 256)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String phone;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    public Client(String firstName, String lastName, String email, String phone, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+}
