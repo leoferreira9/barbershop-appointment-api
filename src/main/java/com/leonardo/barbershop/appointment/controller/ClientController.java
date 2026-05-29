e56df67a9a8f8fb9049a23f70f63655586dcc305
Leonardo <leobarros_@outlook.com>
2026-05-28 21:19:53 -0300

    feat: add Client controller

---
+++ src/main/java/com/leonardo/barbershop/appointment/controller/ClientController.java
@@ -0,0 +1,58 @@
+package com.leonardo.barbershop.appointment.controller;
+
+import com.leonardo.barbershop.appointment.dto.client.ClientRequest;
+import com.leonardo.barbershop.appointment.dto.client.ClientResponse;
+import com.leonardo.barbershop.appointment.dto.client.ClientUpdateRequest;
+import com.leonardo.barbershop.appointment.service.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/clients")
+public class ClientController {
+
+    private final ClientService service;
+
+    public ClientController(ClientService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> create(@RequestBody @Valid ClientRequest request){
+        ClientResponse savedClient = service.create(request);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedClient.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(savedClient);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientResponse> findById(@PathVariable UUID id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientResponse>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientResponse> update(@PathVariable UUID id, @RequestBody @Valid ClientUpdateRequest request){
+        return ResponseEntity.ok(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
