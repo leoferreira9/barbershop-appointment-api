862d061a47073eb460c4b7dfe463743cef6d7358
Leonardo <leobarros_@outlook.com>
2026-07-06 22:52:17 -0300

    feat: add Swagger documentation to Employee DTOs

--- src/main/java/com/leonardo/barbershop/appointment/dto/employee/EmployeePatchRequest.java
+++ src/main/java/com/leonardo/barbershop/appointment/dto/employee/EmployeePatchRequest.java
@@ -1,17 +1,22 @@
 package com.leonardo.barbershop.appointment.dto.employee;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record EmployeePatchRequest(
+
+        @Schema(description = "Employee name", example = "Laura")
         @Size(max = 150)
         String name,
 
+        @Schema(description = "Employee phone number", example = "(00) 00000-0000")
         @Size(max = 20)
         @Pattern(regexp = "^(?:\\+?55\\s?)?(?:\\(?([1-9][0-9])\\)?\\s?)?(?:((?:9\\d|[2-9])\\d{3})\\-?(\\d{4}))$")
         String phone,
 
+        @Schema(description = "Employee email", example = "laura@email.com")
         @Email
         @Size(max = 256)
         String email
--- src/main/java/com/leonardo/barbershop/appointment/dto/employee/EmployeeResponse.java
+++ src/main/java/com/leonardo/barbershop/appointment/dto/employee/EmployeeResponse.java
@@ -1,11 +1,23 @@
 package com.leonardo.barbershop.appointment.dto.employee;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
 public record EmployeeResponse(
+
+        @Schema(description = "Employee ID", example = "810b60ad-e152-4656-a8f5-eb8c4d35633a")
         UUID id,
+
+        @Schema(description = "Employee name", example = "Laura")
         String name,
+
+        @Schema(description = "Employee phone number", example = "(00) 00000-0000")
         String phone,
+
+        @Schema(description = "Employee email", example = "laura@email.com")
         String email,
+
+        @Schema(description = "Employee active", example = "true")
         boolean active
 ) {}
