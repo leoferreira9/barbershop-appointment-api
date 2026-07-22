d2092d840db96042c20b59c19961179fa7bbf13a
Leonardo <leobarros_@outlook.com>
2026-07-21 23:34:54 -0300

    test: add findById not found test to ClientController

--- src/test/java/com/leonardo/barbershop/appointment/controller/ClientControllerTest.java
+++ src/test/java/com/leonardo/barbershop/appointment/controller/ClientControllerTest.java
@@ -1,20 +1,18 @@
 package com.leonardo.barbershop.appointment.controller;
 
 import com.leonardo.barbershop.appointment.dto.client.ClientResponse;
-import com.leonardo.barbershop.appointment.model.Client;
+import com.leonardo.barbershop.appointment.exception.EntityNotFoundException;
 import com.leonardo.barbershop.appointment.service.ClientService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -39,5 +37,19 @@ class ClientControllerTest {
         mvc.perform(
                 get("/api/v1/clients/{id}", id))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnNotFoundWhenClientByIdDoesNotExist() throws Exception {
+        UUID id = UUID.fromString("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+
+        when(clientService.findById(id))
+                .thenThrow(new EntityNotFoundException("Client not found with ID: " + id));
+
+        mvc.perform(
+                get("/api/v1/clients/{id}", id))
+                .andExpect(status().isNotFound());
+
+        verify(clientService).findById(id);
     }
 }
